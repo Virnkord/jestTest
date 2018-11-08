b69c2842c32bdc0690c53e9d60b87c551e36e9bd
Virnkord <ivnmikhailov@gmail.com>
2018-11-08 18:50:48 +0300

    Just a little fix

--- testmodule.test.js
+++ testmodule.test.js
@@ -4,10 +4,12 @@ test("Sqrt function 4 equals 2", () => {
   expect(mysqrt(4)).toBe(2)
 })
 
+test("Sqrt function 36 equals 6", () => {
+  expect(mysqrt(36)).toBe(6)
+})
+
 test("Sqrt function 25 equals 5", () => {
   expect(mysqrt(25)).toBe(5)
 })
 
-test("Sqrt function 36 equals 6", () => {
-  expect(mysqrt(36)).toBe(6)
-})
+
